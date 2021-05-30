5799200616347b86659c3dba1a480abb894cd88b
ulzaman <ulzaman415@gmail.com>
2021-05-31 01:27:30 +0530

    this is first commit

---
+++ src/main/java/Lamdatest_tast/Lamdatest_tast/App.java
@@ -0,0 +1,13 @@
+package Lamdatest_tast.Lamdatest_tast;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
