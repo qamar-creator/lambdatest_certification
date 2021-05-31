e25b82e03fded1c87ff52ad7cf1f1ea1b00eb8db
qamar-creator <ulzaman415@gmail.com>
2021-05-31 19:31:14 +0000

    This is commit for the latest test

---
+++ src/test/Test2.java
@@ -0,0 +1,27 @@
+package lambdatest_p;
+
+//TestNG Todo : Sample App
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class test2
+{
+
+@Test
+public void test_2()
+{
+
+    System.out.println("This is test for java");
+}
+
+}
